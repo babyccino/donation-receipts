e325c7836579296ff9dfc5fe04480d85cf4f36a3
Gus Ryan <gus.ryan163@gmail.com>
2022-01-28 01:17:10 -0800

    refactor

--- index.js
+++ index.js
@@ -1,77 +1,85 @@
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
-const doc = new PDFDocument({
-  size: 'LETTER',
-  margins: {
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  }
-});
+const createDonationReceipt = (filename, receiptNumber, donee, doner, locationIssued, dateIssued = new Date()) => {
+  const doc = new PDFDocument({
+    size: 'LETTER',
+    margins: {
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0
+    }
+  });
 
-doc.pipe(fs.createWriteStream('output.pdf'));
+  doc.pipe(fs.createWriteStream(filename));
 
-const largeFontSize = 15;
-doc
-  .font('./fonts/NotoSans-Regular.ttf')
-  .fontSize(largeFontSize)
-  .text('Official donation receipt for income tax purposes', 15, 20);
+  const largeFontSize = 15;
+  doc
+    .font('./fonts/NotoSans-Regular.ttf')
+    .fontSize(largeFontSize)
+    .text('Official donation receipt for income tax purposes', 15, 20);
 
-let receiptNumber = 1;
+  doc
+    .font('./fonts/NotoSans-Regular.ttf')
+    .fontSize(largeFontSize)
+    .text(`receipt# ${receiptNumber}`, 470, 20);
 
-doc
-  .font('./fonts/NotoSans-Regular.ttf')
-  .fontSize(largeFontSize)
-  .text(`receipt# ${receiptNumber}`, 470, 20);
+  // logo
+  doc.image('./image.jpg', 15, 55, {
+    fit: [60, 60]
+  });
 
-// logo
-doc.image('./image.jpg', 15, 55, {
-  fit: [60, 60]
-});
+  // donee name, address and charity number, left side next to logo
+  const smallFontSize = 10;
+  doc
+    .font('./fonts/NotoSans-Regular.ttf')
+    .fontSize(smallFontSize)
+    .text(donee.name, 80, 60);
+  doc
+    .font('./fonts/NotoSans-Regular.ttf')
+    .fontSize(smallFontSize)
+    .text(donee.address);
+  doc
+    .font('./fonts/NotoSans-Regular.ttf')
+    .fontSize(smallFontSize)
+    .text(`Charitable registration #: ${donee.charityNumber}`);
 
-// donee name, address and charity number, left side next to logo
-const smallFontSize = 10;
-let doneeName = 'Squamish United Church',
-    doneeAddress = 'Box 286',
-    charityNumber = '000000000 RR 0000';
-doc
-  .font('./fonts/NotoSans-Regular.ttf')
-  .fontSize(smallFontSize)
-  .text(doneeName, 80, 60);
-doc
-  .font('./fonts/NotoSans-Regular.ttf')
-  .fontSize(smallFontSize)
-  .text(doneeAddress);
-doc
-  .font('./fonts/NotoSans-Regular.ttf')
-  .fontSize(smallFontSize)
-  .text(`Charitable registration #: ${charityNumber}`);
+  // date and location issued right side
+  doc
+    .font('./fonts/NotoSans-Regular.ttf')
+    .fontSize(smallFontSize)
+    .text(`Receipt issued: ${dateIssued.getDate()}/${dateIssued.getMonth()}/${dateIssued.getFullYear()}`, 380, 60);
+  doc.moveDown();
+  doc
+    .font('./fonts/NotoSans-Regular.ttf')
+    .fontSize(smallFontSize)
+    .text(`Location Issued: ${locationIssued}`);
 
-// date and location issued right side
-let dateIssued = '1/2/2022',
-    locationIssued = 'Squamish, BC';
-doc
-  .font('./fonts/NotoSans-Regular.ttf')
-  .fontSize(smallFontSize)
-  .text(`Receipt issued: ${dateIssued}`, 380, 60);
-doc.moveDown();
-doc
-  .font('./fonts/NotoSans-Regular.ttf')
-  .fontSize(smallFontSize)
-  .text(`Location Issued: ${locationIssued}`);
+  // donated by, address
+  doc
+    .font('./fonts/NotoSans-Regular.ttf')
+    .fontSize(smallFontSize)
+    .text(`Donated by: ${doner.name}`, 15, 130);
+  doc
+    .font('./fonts/NotoSans-Regular.ttf')
+    .fontSize(smallFontSize)
+    .text(`Address: ${doner.address}`, 306, 130);
 
-// donated by, address
-let donerName = 'Jeff Jefferson',
-    donerAddress = 'McNamee Pl';
-doc
-  .font('./fonts/NotoSans-Regular.ttf')
-  .fontSize(smallFontSize)
-  .text(`Donated by: ${donerName}`, 15, 130);
-doc
-  .font('./fonts/NotoSans-Regular.ttf')
-  .fontSize(smallFontSize)
-  .text(`Address: ${donerAddress}`, , 130);
+  doc.end();
+}
 
-doc.end();
+const filename = 'output.pdf',
+      receiptNumber = 1,
+      donee = {
+  name: 'Squamish United Church',
+  address: 'Box 286',
+  charityNumber: '000000000 RR 0000'
+},
+      doner = {
+  name: 'Jeff Jefferson',
+  donerAddress: 'McNamee Pl'
+},
+locationIssued = 'Squamish, BC';
+
+createDonationReceipt(filename, receiptNumber, donee, doner, locationIssued);
